c124d7ccfb6b466e36cbf81bf1ef0a2251dfc15c
yuki takagi <yuki99takagi@gmail.com>
2020-09-20 20:59:17 +0900

    Add: chapter2 リスト要素のプロパティを更新する

--- chapter2/main.js
+++ chapter2/main.js
@@ -1,7 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        name: 'キマイラ',
         list: [
             { id: 1, name: 'スライム', hp: 100 },
             { id: 2, name: 'ゴブリン', hp: 200 },
@@ -9,10 +8,9 @@ var app = new Vue({
         ]
     },
     methods: {
-        // 削除ボタンをクリックした時のハンドラ
-        doRemove: function (index) {
-            // 受け取ったインデックスの位置から1個要素を削除
-            this.list.splice(index, 1)
+        // ボタンのクリックイベントのハンドラ
+        doAttack: function (index) {
+            this.list[index].hp -= 10
         }
     }
 })
