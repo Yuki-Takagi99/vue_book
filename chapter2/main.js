ecd79061c0d6c065b701ac6fc7cb5cc636c6c7f8
yuki takagi <yuki99takagi@gmail.com>
2020-09-20 18:44:14 +0900

    Add: chapter2 リストにデータを追加する

--- chapter2/main.js
+++ chapter2/main.js
@@ -1,6 +1,26 @@
 var app = new Vue({
     el: '#app',
     data: {
-        radius: 50
+        name: 'キマイラ',
+        list: [
+            { id: 1, name: 'スライム', hp: 100 },
+            { id: 2, name: 'ゴブリン', hp: 200 },
+            { id: 3, name: 'ドラゴン', hp: 500 }
+        ]
+    },
+    methods: {
+        // 追加ボタンをクリックした時のハンドラ
+        doAdd: function () {
+            // リスト内で一番大きいIDを取得
+            var max = this.list.reduce(function(a,b) {
+                return a > b.id ? a : b.id
+            }, 0)
+            // 新しいモンスターをリストに追加
+            this.list.push({
+                id: max + 1,
+                name: this.name,
+                hp: 500
+            })
+        }
     }
 })
