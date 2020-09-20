77f48cd43e8e8f1e1c107aa97e9a0aec27f05a45
yuki takagi <yuki99takagi@gmail.com>
2020-09-20 16:56:01 +0900

    Add: chapter2 クラスとスタイルのデータバインディング

--- chapter2/main.js
+++ chapter2/main.js
@@ -1,12 +1,13 @@
 var app = new Vue({
     el: '#app',
     data: {
-        count: 0
-    },
-    methods: {
-        // ボタンをクリックした時のハンドラ
-        increment: function () {
-            this.count += 1 // 処理は再代入するだけでOK!
+        classObject: {
+            child: true,
+            'is-active': false
+        },
+        styleObject: {
+            color: 'red',
+            backgroundColor: 'lightgray'
         }
     }
 })
