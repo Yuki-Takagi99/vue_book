7c93127c7f97522bf87926023e648063a6da9268
yuki takagi <yuki99takagi@gmail.com>
2020-09-21 11:53:15 +0900

    Add: chapter2 テンプレート制御ディレクティブ

--- chapter2/main.js
+++ chapter2/main.js
@@ -1,16 +1,6 @@
-var app = new Vue({
+new Vue({
     el: '#app',
     data: {
-        list: [
-            { id: 1, name: 'スライム', hp: 100 },
-            { id: 2, name: 'ゴブリン', hp: 200 },
-            { id: 3, name: 'ドラゴン', hp: 500 }
-        ]
-    },
-    methods: {
-        // ボタンのクリックイベントのハンドラ
-        doAttack: function (index) {
-            this.list[index].hp -= 10
-        }
+        message: 'Hello <strong>Vue.js!</strong>'
     }
 })
