3eb3067d8e9180d2438bcea8723d0a75369ec329
yuki takagi <yuki99takagi@gmail.com>
2020-09-22 15:22:40 +0900

    Add: chapter4 フォームを監視してGitHubAPIからデータを取得する

--- chapter4/main.js
+++ chapter4/main.js
@@ -1,34 +1,21 @@
 new Vue({
     el: '#app',
     data: {
-        order: false,
-        // フォームの入力と紐づけるデータ
-        budget: 300,
-        // 表示件数
-        limit: 2,
-        // もとになるリスト
-        list: [
-            { id: 1, name: 'りんご', price: 100 },
-            { id: 2, name: 'ばなな', price: 200 },
-            { id: 3, name: 'いちご', price: 400 },
-            { id: 4, name: 'オレンジ', price: 300 },
-            { id: 5, name: 'メロン', price: 500 },
+        list: [],
+        current: '',
+        topics: [
+            { value: 'vue', name: 'Vue.js' },
+            { value: 'jQuery', name: 'jQuery' }
         ]
     },
-    computed: {
-        // budget以下のリストを返す算出プロパティ
-        matched: function () {
-            return this.list.filter(function (el) {
-                return el.price <= this.budget
-            }, this)
-        },
-        // sortedを新しく追加
-        sorted: function () {
-            return _.orderBy(this.matched, 'price', this.order ? 'desc' : 'asc')
-        },
-        // limitedで使用するリストをsortedに変更
-        limited: function () {
-            return this.sorted.slice(0, this.limit)
+    watch: {
+        current: function (val) {
+            // GitHubのAPIからトピックのリポジトリを検索
+            axios.get('https://api.github.com/search/repositories', {
+                params: { q: 'topic:' + val }
+            }).then(function (response) {
+                this.list = response.data.items
+            }.bind(this))
         }
-    }
+    },
 })
