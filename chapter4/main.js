4e396dc2d15341505cb45591039b498fd125da7a
yuki takagi <yuki99takagi@gmail.com>
2020-09-22 14:09:53 +0900

    Add: chapter4 算出プロパティのゲッターとセッター

--- chapter4/main.js
+++ chapter4/main.js
@@ -2,21 +2,12 @@ new Vue({
     el: '#app',
     data: {
         width: 800,
-        height: 600
     },
     computed: {
-        halfWidth: function () {
-            return this.width / 2
-        },
-        halfHeight: function () {
-            return this.height / 2
-        },
-        // 「widt * height」の中心座標をオブジェクトで返す
-        halfPoint: function () {
-            return {
-                x: this.halfWidth,
-                y: this.halfHeight
-            }
+        halfWidth: {
+            get: function () { return this.width / 2 },
+            // halfWidthの2倍の数値をwidthに代入する
+            set: function (val) { this.width = val * 2 },
         }
     }
 })
