5d7a419a58cc462b699cb3b95e5324e5b09a059f
yuki takagi <yuki99takagi@gmail.com>
2020-09-23 17:20:28 +0900

    Add: chapter5 親コンポーネントから子コンポーネントへの通信

--- chapter5/main.js
+++ chapter5/main.js
@@ -1,9 +1,14 @@
-var myComponent = {
-    template: '<p>MyComponent</p>'
-}
+Vue.component('comp-child', {
+    // テンプレートで受け取ったvalを使用
+    template: '<p>{{ val }}</p>',
+    // 受け取る属性名を指定
+    props: ['val']
+})
+
 new Vue({
     el: '#app',
-    components: {
-        'my-component': myComponent
+    data: {
+        valueA: 'これは子A',
+        valueB: 'これは子B'
     }
 })
