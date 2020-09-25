213267181c49f44b77dd939230490e668b774956
yuki takagi <yuki99takagi@gmail.com>
2020-09-25 15:39:00 +0900

    Add: chapter6 SVGのトランジション

--- chapter6/main.js
+++ chapter6/main.js
@@ -1,18 +1,15 @@
+Vue.component('my-circle', {
+    template: '<circle cx="80" cy="75" r="50" v-bind:fill="fill"/>',
+    props: { fill: String }
+})
 new Vue({
     el: '#app',
     data: {
-        order: false,
-        list: [
-            { id: 1, name: 'りんご', price: 100 },
-            { id: 2, name: 'ばなな', price: 200 },
-            { id: 3, name: 'いちご', price: 500 }
-        ]
+        toggle: false
     },
     computed: {
-        // orderの値でリストの順番を反転する算出プロパティ
-        sortedList: function () {
-            // LodashのorderByメソッドを使用
-            return _.orderBy(this.list, 'price', this.order ? 'desc' : 'asc')
+        fill: function () {
+            return this.toggle ? 'lightpink' : 'skyblue'
         }
     }
 })
