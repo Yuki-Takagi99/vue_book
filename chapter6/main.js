58aecd0f2c83be9abe7600bef4218d5fd1597349
yuki takagi <yuki99takagi@gmail.com>
2020-09-26 22:03:24 +0900

    Add: chapter6 トランジションフック

--- chapter6/main.js
+++ chapter6/main.js
@@ -1,15 +1,37 @@
-Vue.component('my-circle', {
-    template: '<circle cx="80" cy="75" r="50" v-bind:fill="fill"/>',
-    props: { fill: String }
-})
 new Vue({
     el: '#app',
     data: {
-        toggle: false
+        show: true
     },
-    computed: {
-        fill: function () {
-            return this.toggle ? 'lightpink' : 'skyblue'
+    methods: {
+        // Enter
+        beforeEnter: function (el) {
+            console.log('before-enter')
+        },
+        enter: function (el, done) {
+            console.log('enter')
+            setTimeout(done, 1000)
+        },
+        afterEnter: function (el) {
+            console.log('after-enter')
+        },
+        enterCancelled: function (el) {
+            console.log('enter-cancelled')
+        },
+        // Leave
+        beforeLeave: function (el) {
+            console.log('before-leave')
+        },
+        leave: function (el, done) {
+            console.log('leave')
+            setTimeout(done, 1000)
+        },
+        afterLeave: function (el) {
+            console.log('after-leave')
+        },
+        // v-show と共に使うときだけ leaveCancelled は有効です
+        leaveCancelled: function (el) {
+            console.log('leave-cancelled')
         }
     }
 })
